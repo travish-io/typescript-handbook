1093534c4c13c8ba6b33ce625fcec7f673ac66a2
Travis Hall <t.hall89d@gmail.com>
2022-01-21 12:49:00 -0500

    initial commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+// Greets the world.
+console.log("Hello world!");
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+}
+greet("Brendan", Date.now());
---
+++ hello.ts
@@ -0,0 +1,8 @@
+export {};
+
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet("Brendan", "1/21/2022");
