b8dfab48bbea8d8a2b781ff2b9a980d8a1a3577c
Travis Hall <t.hall89d@gmail.com>
2022-01-22 10:47:21 -0500

    downleveling

--- hello.js
+++ hello.js
@@ -1,8 +1,9 @@
+"use strict";
+exports.__esModule = true;
 // This is an industrial-grade general-purpose greeter function:
 function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
 }
 greet("Brendan", new Date());
 // inferred types
-let msg = "hello there!";
-export {};
+var msg = "hello there!";
